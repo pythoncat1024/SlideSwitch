5aec172bdceeb7458ca891279f9cbaf88c1b77ad
pythoncat <pythoncat@qq.com>
2017-04-30 09:41:51 +0800

    + 添加依赖 logutils / SlideSwitch

---
+++ slideswitchlib/src/main/java/com/python/cat/slideswitchlib/SlideSwitch.java
@@ -0,0 +1,27 @@
+package com.python.cat.slideswitchlib;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * packageName: com.python.cat.slideswitchlib
+ * Created on 2017/4/30.
+ *
+ * @author cat
+ */
+
+public class SlideSwitch extends View {
+    public SlideSwitch(Context context) {
+        super(context);
+    }
+
+    public SlideSwitch(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SlideSwitch(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
