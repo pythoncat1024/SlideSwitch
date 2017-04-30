b2436c35d6b02e421ab9615a03ae23efc9c7d4f6
pythoncat <pythoncat@qq.com>
2017-04-30 10:18:18 +0800

    * 处理触摸滑动事件 1

--- slideswitchlib/src/main/java/com/python/cat/slideswitchlib/SlideSwitch.java
+++ slideswitchlib/src/main/java/com/python/cat/slideswitchlib/SlideSwitch.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -16,8 +17,22 @@ import android.view.View;
  */
 
 public class SlideSwitch extends View {
+
+    private int currentX;
+    private boolean isTouching = false;
+
+    private enum State {
+        OPEN, CLOSE
+    }
+
     private Bitmap mBackBmp;
     private Bitmap mFrontBmp;
+
+    private State switchState = State.CLOSE;
+
+    public void setSwitchState(State state) {
+        this.switchState = state;
+    }
 
     public void setBackGroundBmp(int resId) {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resId);
@@ -59,8 +74,55 @@ public class SlideSwitch extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(mBackBmp, 0, 0, null);
-        canvas.drawBitmap(mFrontBmp, 0, 0, null);
+
+        int backGroundWidth = mBackBmp.getWidth();
+        int frontWidth = mFrontBmp.getWidth();
+        if (isTouching) {
+            switch (switchState) {
+                case OPEN:
+                    // todo
+                    break;
+                case CLOSE:
+                default:
+                    // todo
+                    if (currentX < frontWidth / 2) {
+                        canvas.drawBitmap(mFrontBmp, 0, 0, null);
+                    } else if (currentX > backGroundWidth - frontWidth / 2) {
+                        canvas.drawBitmap(mFrontBmp, backGroundWidth - frontWidth, 0, null);
+                    } else {
+                        canvas.drawBitmap(mFrontBmp, currentX - frontWidth / 2, 0, null);
+                    }
+
+                    break;
+            }
+        } else {
+            if (currentX < backGroundWidth / 2) {
+                canvas.drawBitmap(mFrontBmp, 0, 0, null);
+            } else {
+                canvas.drawBitmap(mFrontBmp, backGroundWidth - frontWidth, 0, null);
+            }
+        }
+
     }
 
-
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                isTouching = true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                isTouching = false;
+                break;
+            default:
+                break;
+        }
+        currentX = Math.round(event.getX());
+        invalidate();
+        return true;
+    }
 }
