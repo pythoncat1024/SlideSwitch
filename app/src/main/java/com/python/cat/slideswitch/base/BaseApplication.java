d23f96610d40f56fba7431cd1971e93851819f4d
pythoncat <pythoncat@qq.com>
2017-04-30 09:40:04 +0800

    + 为每个module 创建 Application 类 ，并且需要在 主 module 的<application>下添加属性：tools:replace="android:name"

---
+++ app/src/main/java/com/python/cat/slideswitch/base/BaseApplication.java
@@ -0,0 +1,17 @@
+package com.python.cat.slideswitch.base;
+
+import android.app.Application;
+
+/**
+ * packageName: com.python.cat.slideswitch.base
+ * Created on 2017/4/30.
+ *
+ * @author cat
+ */
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ slideswitchlib/src/main/java/com/python/cat/slideswitchlib/base/SlideSwitchApplication.java
@@ -0,0 +1,17 @@
+package com.python.cat.slideswitchlib.base;
+
+import android.app.Application;
+
+/**
+ * packageName: com.python.cat.slideswitchlib.base
+ * Created on 2017/4/30.
+ *
+ * @author cat
+ */
+
+public class SlideSwitchApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
