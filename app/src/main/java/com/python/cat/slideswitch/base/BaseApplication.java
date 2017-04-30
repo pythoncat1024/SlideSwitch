e93bd70675d20443c224fb26ea0b7d07df432027
pythoncat <pythoncat@qq.com>
2017-04-30 09:59:13 +0800

    * 静态View 绘制完成

--- app/src/main/java/com/python/cat/slideswitch/base/BaseApplication.java
+++ app/src/main/java/com/python/cat/slideswitch/base/BaseApplication.java
@@ -1,6 +1,10 @@
 package com.python.cat.slideswitch.base;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+import android.os.Bundle;
 
 /**
  * packageName: com.python.cat.slideswitch.base
@@ -13,5 +17,62 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        register();
+    }
+
+    private void register() {
+        registerComponentCallbacks(new ComponentCallbacks() {
+            @Override
+            public void onConfigurationChanged(Configuration newConfig) {
+
+            }
+
+            @Override
+            public void onLowMemory() {
+
+            }
+        });
+        registerOnProvideAssistDataListener(new OnProvideAssistDataListener() {
+            @Override
+            public void onProvideAssistData(Activity activity, Bundle data) {
+
+            }
+        });
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
     }
 }
--- slideswitchlib/src/main/java/com/python/cat/slideswitchlib/SlideSwitch.java
+++ slideswitchlib/src/main/java/com/python/cat/slideswitchlib/SlideSwitch.java
@@ -1,6 +1,9 @@
 package com.python.cat.slideswitchlib;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,6 +16,23 @@ import android.view.View;
  */
 
 public class SlideSwitch extends View {
+    private Bitmap mBackBmp;
+    private Bitmap mFrontBmp;
+
+    public void setBackGroundBmp(int resId) {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resId);
+        if (bitmap != null) {
+            this.mBackBmp = bitmap;
+        }
+    }
+
+    public void setFrontBmp(int resId) {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resId);
+        if (bitmap != null) {
+            this.mFrontBmp = bitmap;
+        }
+    }
+
     public SlideSwitch(Context context) {
         this(context, null);
     }
@@ -23,6 +43,23 @@ public class SlideSwitch extends View {
 
     public SlideSwitch(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        this.mFrontBmp = BitmapFactory.decodeResource(getResources(), R.drawable.slide_front);
+        this.mBackBmp = BitmapFactory.decodeResource(getResources(), R.drawable.switch_background);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(mBackBmp.getWidth(), mBackBmp.getHeight());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(mBackBmp, 0, 0, null);
+        canvas.drawBitmap(mFrontBmp, 0, 0, null);
     }
 
 
